80eadbeac603d6fc917eea6c0aa92577c4297998
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-10-24 01:36:41 -0400

    completed #1

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # maps_travel
 
 - [ ] Ability to add markers on a map 
+    - Coordinates have to be flipped; API sends an array, like with long&lat. However geocodes are written as lat&long
--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 // import '/node_modules/leaflet/dist/leaflet.js'
 // import L from 'leaflet'
 
-var map = L.map('map').setView([43.65, -79.39], 13);
+// require('dotenv').config()
+
+var map = L.map('map').setView([43.65, -79.39], 1);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -10,11 +12,39 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 
-const mapbox_accessToken = 'pk.eyJ1IjoiYXNhZHMyMyIsImEiOiJjbG1ud29wbDQwdmR5MnRsMTlxNHV3eGQyIn0.LR7dYh818Ya5Ls9iTmCJ_Q'
+var formElement = document.getElementById('locationInput')
+formElement.addEventListener('submit', (event) => {
+    event.preventDefault()
 
-// var marker = L.circle([51.508, -0.11], {
-//   color: 'red',
-//   fillColor: '#f03',
-//   fillOpacity: 0.5,
-//   radius: 500
-// }).addTo(map);
+    const formData = new FormData(event.target)
+    const refinedData= Object.fromEntries(formData.entries())
+
+    // console.log(mapbox_accessToken)
+    
+    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${refinedData.country}.json?access_token=pk.eyJ1IjoiYXNhZHMyMyIsImEiOiJjbG1ud2pyZzkwem95MmpuNXdxNW1wczE1In0.rVhuUWrEFEMpYFyaaVmt4Q`)
+    .then(res => res.json())
+    .then(res => {
+
+        console.log(res)
+        const coordinates = res.features[0].geometry.coordinates
+        console.log(coordinates)
+
+        L.marker([coordinates[1], coordinates[0]]).addTo(map)
+
+        map.setView([coordinates[1], coordinates[0]], 1)
+
+    })
+    .catch(err => console.log(err))
+
+
+})
+
+
+
+
+var marker = L.circle([43.65, -79.39], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 500
+}).addTo(map);
