7046eabcb86bd13d5cc2ee7a99192f5b1e780f30
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-11-05 15:44:32 -0500

    minor changes

--- app.js
+++ app.js
@@ -1,49 +1,93 @@
-require('dotenv').config()
+// import "./node_modules/leaflet/dist/leaflet.css"; // Import Leaflet's CSS
+import "./node_modules/leaflet/dist/leaflet.js"; // Import Leaflet
 
-const apiKey=process.env.PUBLIC
+// let dotenv = require("dotenv").config();
+// let ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 
+let COORDINATES_TORONTO = [43.65, -79.39];
+let ACCESS_TOKEN =
+  "pk.eyJ1IjoiYXNhZHMyMyIsImEiOiJjbG1ud2pyZzkwem95MmpuNXdxNW1wczE1In0.rVhuUWrEFEMpYFyaaVmt4Q";
 
-var map = L.map('map').setView([43.65, -79.39], 1);
+let map = L.map("map").setView(COORDINATES_TORONTO, 2);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
+let formElement = document.getElementById("locationInput");
+formElement.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-var formElement = document.getElementById('locationInput')
-formElement.addEventListener('submit', (event) => {
-    event.preventDefault()
+  const formData = new FormData(event.target);
+  const refinedData = Object.fromEntries(formData.entries());
 
-    const formData = new FormData(event.target)
-    const refinedData= Object.fromEntries(formData.entries())
+  fetch(
+    `https://api.mapbox.com/geocoding/v5/mapbox.places/${refinedData.country}.json?access_token=${ACCESS_TOKEN}`
+  )
+    .then((res) => res.json())
+    .then((res) => {
+      const coordinates = res.features[0].geometry.coordinates;
 
-    // console.log(mapbox_accessToken)
-    
-    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${refinedData.country}.json?access_token=${apiKey}`)
-    .then(res => res.json())
-    .then(res => {
+      L.marker([coordinates[1], coordinates[0]]).addTo(map);
 
-        console.log(res)
-        const coordinates = res.features[0].geometry.coordinates
-        console.log(coordinates)
-
-        L.marker([coordinates[1], coordinates[0]]).addTo(map)
-
-        map.setView([coordinates[1], coordinates[0]], 1)
-
+      map.setView([coordinates[1], coordinates[0]], 1);
     })
-    .catch(err => console.log(err))
+    .catch((err) => console.log(err));
+});
 
-
-})
-
-
-
-
-var marker = L.circle([43.65, -79.39], {
-  color: 'red',
-  fillColor: '#f03',
+let marker = L.circle([43.65, -79.39], {
+  color: "red",
+  fillColor: "#f03",
   fillOpacity: 0.5,
-  radius: 500
+  radius: 500,
 }).addTo(map);
+
+// -----------------------
+
+// function initializeMap() {
+//   const map = L.map("map").setView(COORDINATES_TORONTO, 2);
+//   L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+//     maxZoom: 19,
+//     attribution:
+//       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+//   }).addTo(map);
+//   return map;
+// }
+
+// function addMarkerAndZoom(map, coordinates) {
+//   L.marker(coordinates).addTo(map);
+//   map.setView(coordinates, 1);
+// }
+
+// function fetchAndAddMarker(map) {
+//   const formElement = document.getElementById("locationInput");
+//   formElement.addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     const formData = new FormData(event.target);
+//     const refinedData = Object.fromEntries(formData.entries());
+//     fetch(
+//       `https://api.mapbox.com/geocoding/v5/mapbox.places/${refinedData.country}.json?access_token=${ACCESS_TOKEN}`
+//     )
+//       .then((res) => res.json())
+//       .then((res) => {
+//         const coordinates = res.features[0].geometry.coordinates;
+//         addMarkerAndZoom(map, [coordinates[1], coordinates[0]]);
+//       })
+//       .catch((err) => console.log(err));
+//   });
+// }
+
+// function createMarkerCircle(map, coordinates) {
+//   L.circle(coordinates, {
+//     color: "red",
+//     fillColor: "#f03",
+//     fillOpacity: 0.5,
+//     radius: 500,
+//   }).addTo(map);
+// }
+
+// const map = initializeMap();
+// fetchAndAddMarker(map);
+// createMarkerCircle(map);
